aad4c07927e87517133fc5f4e671ab28c6501663
Arjun Arasu <90092043+arjunultra@users.noreply.github.com>
2022-12-19 05:59:42 +0530

    screenshot wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// prettier-ignore
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('../assets/screenshot.png')",
+      },
+    },
   },
   plugins: [],
 };
