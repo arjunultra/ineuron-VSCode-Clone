167eccf4c6cfd698a5c7675f32d8532d872739c7
Arjun Arasu <90092043+arjunultra@users.noreply.github.com>
2022-12-21 22:12:07 +0530

    code restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // prettier-ignore
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
